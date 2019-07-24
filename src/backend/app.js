1af2e4a6ff6054131d34b14217e5fdada29a93dd
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-07-24 11:40:10 +0100

    Attempt to debug why Google Login on frontend is failing

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3001;
@@ -20,12 +22,17 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });   
 
-app.post('/rest/signin', (req, res) => {
-    let parsed= JSON.parsed(req)
-    let token = jwtSimple.decode(parsed.hg.id_token);
-    let response = {token, hd: 'https://kung-who.herokuapp.com/', secret: 'gmRXBkluAd9oG_fvQGHJjMTP'}
-    console.log(response)
-    res.send(response)
+app.post('/rest/login', jsonParser, (req, res) => {
+    console.log("Request is: ", req);
+    // console.log(req.body);
+    // console.log(parsed);
+    res.end(JSON.stringify({
+        result: 'Hi'
+    }));
+    // let token = jwtSimple.decode(parsed.hg.id_token);
+    // let response = {token, hd: 'https://kung-who.herokuapp.com/', secret: 'gmRXBkluAd9oG_fvQGHJjMTP'}
+    // console.log(response)
+    // res.send(response)
 }); 
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
