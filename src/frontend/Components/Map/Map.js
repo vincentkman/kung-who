25170ef23ba335f3cb9ea02e5852c8237fccf764
Vincent Man <vincentman@vincents-mbp.home>
2019-07-25 01:04:42 +0100

    deploy

--- src/frontend/Components/Map/Map.js
+++ src/frontend/Components/Map/Map.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Map.scss';
 
 function Map(props) {
-    const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+    // const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+    const API_KEY = "AIzaSyA2mnrO-E4fx-eXWvqFrYbsVMxeF8le030";
     const mapUrl = `https://www.google.com/maps/embed/v1/search?q=${props.map}&key=${API_KEY}`
     return (
         <div className='Map'>
